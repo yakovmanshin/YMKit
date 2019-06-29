62bd9fb64a18aa99b2f75df54de6d4a8ed6639da
Yakov Manshin <git@yakovmanshin.com>
2019-06-29 17:41:09 +0300

    YMKit / DeviceInfo
    * Added a new struct, DeviceInfo, for retrieving information about the device

--- YMKit/Controllers/YMKit.swift
+++ YMKit/Controllers/YMKit.swift
@@ -29,17 +29,17 @@ final public class YMKit {
 }
 
 extension YMKit {
-    public struct __DeviceInfo {
+    public struct DeviceInfo {
+        
+        private init() { }
         
         public static var model: String {
             return UIDevice.current.model
         }
         
-        public static var name: String {
-            return UIDevice.current.name
-        }
-        
         public struct OS {
+            
+            private init() { }
             
             public static var name: String {
                 return UIDevice.current.systemName
