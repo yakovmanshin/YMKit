75db80fb437ba4b4d36b34d5283eed1f786ed6d6
Yakov Manshin <git@yakovmanshin.com>
2019-08-11 12:27:40 +0300

    YMKit.AppInfo.name
    * Introduced a new computed property for accessing name of the main app

--- YMKit/Controllers/YMKit.swift
+++ YMKit/Controllers/YMKit.swift
@@ -46,6 +46,11 @@ extension YMKit {
             return Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String
         }
         
+        /// Returns display name (product name) of the main app.
+        public static var name: String? {
+            return Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String
+        }
+        
         /// Returns version number of currently installed main app (the one YMKit is embedded into).
         public static var version: String? {
             return Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
