a66e08edf49b9e0623956a71b1922a3cffd8948a
Yakov Manshin <git@yakovmanshin.com>
2019-11-16 23:09:15 +0300

    Update YMFormatter.swift

--- YMKit/Controllers/YMFormatter/YMFormatter.swift
+++ YMKit/Controllers/YMFormatter/YMFormatter.swift
@@ -130,46 +130,45 @@ extension YMFormatter {
         currencySymbol: String? = nil,
         configurationClosure: (NumberFormatter) -> Void = { _ in }
     ) -> String? {
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .currency
-        numberFormatter.usesGroupingSeparator = groupingSeparatorIsEnabled
-        
-        numberFormatter.locale = locale
-        
-        if let currencyCode = currencyCode {
-            numberFormatter.currencyCode = currencyCode
-        }
-        
-        if let currencySymbol = currencySymbol {
-            numberFormatter.currencySymbol = currencySymbol
-        }
-        
-        if significantDigits {
-            numberFormatter.usesSignificantDigits = true
+        return YMFormatter.Number(inputValue).makeString { numberFormatter in
+            numberFormatter.numberStyle = .currency
+            numberFormatter.usesGroupingSeparator = groupingSeparatorIsEnabled
             
-            if let minSignificantDigits = minSignificantDigits {
-                numberFormatter.minimumSignificantDigits = minSignificantDigits
+            numberFormatter.locale = locale
+            
+            if let currencyCode = currencyCode {
+                numberFormatter.currencyCode = currencyCode
             }
             
-            if let maxSignificantDigits = maxSignificantDigits {
-                numberFormatter.maximumSignificantDigits = maxSignificantDigits
+            if let currencySymbol = currencySymbol {
+                numberFormatter.currencySymbol = currencySymbol
             }
-        } else {
-            numberFormatter.usesSignificantDigits = false
-            numberFormatter.minimumIntegerDigits = 1
             
-            if let fixedFractionDigits = fixedFractionDigits {
-                numberFormatter.minimumFractionDigits = fixedFractionDigits
-                numberFormatter.maximumFractionDigits = fixedFractionDigits
+            if significantDigits {
+                numberFormatter.usesSignificantDigits = true
+                
+                if let minSignificantDigits = minSignificantDigits {
+                    numberFormatter.minimumSignificantDigits = minSignificantDigits
+                }
+                
+                if let maxSignificantDigits = maxSignificantDigits {
+                    numberFormatter.maximumSignificantDigits = maxSignificantDigits
+                }
             } else {
-                numberFormatter.minimumFractionDigits = minFractionDigits
-                numberFormatter.maximumFractionDigits = maxFractionDigits
+                numberFormatter.usesSignificantDigits = false
+                numberFormatter.minimumIntegerDigits = 1
+                
+                if let fixedFractionDigits = fixedFractionDigits {
+                    numberFormatter.minimumFractionDigits = fixedFractionDigits
+                    numberFormatter.maximumFractionDigits = fixedFractionDigits
+                } else {
+                    numberFormatter.minimumFractionDigits = minFractionDigits
+                    numberFormatter.maximumFractionDigits = maxFractionDigits
+                }
             }
+            
+            configurationClosure(numberFormatter)
         }
-        
-        configurationClosure(numberFormatter)
-        
-        return numberFormatter.string(from: inputValue)
     }
     
 }
