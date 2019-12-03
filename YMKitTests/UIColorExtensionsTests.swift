95056fe5e91172da3f59db50e6261220dbf6f7de
Yakov Manshin <git@yakovmanshin.com>
2019-12-03 21:34:43 +0300

    UIColor+Extensions Tests Core

---
+++ YMKitTests/UIColorExtensionsTests.swift
@@ -0,0 +1,14 @@
+//
+//  UIColorExtensionsTests.swift
+//  YMKitTests
+//
+//  Created by Yakov Manshin on 12/3/19.
+//  Copyright © 2019 Yakov Manshin. All rights reserved.
+//
+
+import XCTest
+@testable import YMKit
+
+// MARK: - UIColor+Extensions Tests Core
+
+final class UIColorExtensionsTests: XCTestCase { }
