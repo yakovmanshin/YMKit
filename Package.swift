406dd36794a0f55e6ee344dfd5c5a4f9480dce2a
Yakov Manshin <git@yakovmanshin.com>
2020-11-11 21:27:37 +0300

    Removed Extra Comments from Package.swift

--- Package.swift
+++ Package.swift
@@ -8,15 +8,12 @@ let package = Package(
         .iOS(.v8)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "YMKit",
             targets: ["YMKit"]
         )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "YMKit",
             path: "YMKit"
