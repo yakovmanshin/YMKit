eb35d4e36a4c4f52172d47067bc0819d6f09d689
Yakov Manshin <git@yakovmanshin.com>
2021-03-20 12:56:35 +0300

    Added YMKit Exports

---
+++ Sources/YMKit/YMKit.swift
@@ -0,0 +1,9 @@
+//
+//  YMKit.swift
+//  YMKit
+//
+//  Created by Yakov Manshin on 3/10/21.
+//  Copyright © 2021 Yakov Manshin. See the LICENSE file for license info.
+//
+
+@_exported import YMKitInfo
